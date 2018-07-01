985dc28cf3e78f29724ad60979234399a685adbf
Matt Dray <mwdray@gmail.com>
2018-07-01 20:06:25 +0100

    add exclusivity and join

--- 01_get-data.R
+++ 01_get-data.R
@@ -11,35 +11,34 @@ library(rvest)  # webscrape
 library(dplyr)  # data manipulation
 library(tibble)  # tidy tables
 library(stringr)  # string manipulation
+library(janitor)  # clean names
 
 
 # Get catch rates ---------------------------------------------------------
 
 
 # read the html from the bulbapedia page about pokemon catch rates
-
-bulb_page <- read_html(
-  "https://bulbapedia.bulbagarden.net/wiki/List_of_Pok%C3%A9mon_by_catch_rate"
-)
-
 # isolate the tables from the webpage as a list
 
-bulb_tables <- bulb_page %>% html_table(fill = TRUE)
+catch_tables <- read_html(
+  "https://bulbapedia.bulbagarden.net/wiki/List_of_Pok%C3%A9mon_by_catch_rate"
+) %>% 
+  html_table(fill = TRUE)
 
 # isolate the second table from the list (contains catch rates)
 # select certain columns (number, pokemon name, catch rate) and rows (gen I)
 
-catch_rates <- bulb_tables[[2]][c(1, 3, 4)] %>% 
+catch_rates <- catch_tables[[2]][c(1, 3, 4)] %>% 
   rename(
     number = `#`,
     name = Name,
     catch_rate = "Catch rate"
   ) %>% 
-  filter(number < 152) %>%  # first gen pokemon only
+  filter(number < 152) %>%  # gen I pokemon only
   mutate(
     catch_rate = str_replace(catch_rate, "[*]", ""),  # remove asterisks
     catch_rate = as.integer(catch_rate)  # convert to numeric
-    )
+  )
 
 
 # Get rarity --------------------------------------------------------------
@@ -47,33 +46,52 @@ catch_rates <- bulb_tables[[2]][c(1, 3, 4)] %>%
 
 # rarity data from the pokemon revolution online game
 # download the html for the wiki page on rarity
-
-pro_html <- read_html(
-  "https://prowiki.info/index.php?title=List_of_Pok%C3%A9mon_by_Rarity_Tier"
-)
-
 # extract the tables from the page
 
-pro_tables <- pro_html %>% html_table(fill = TRUE)
+rare_tables <- read_html(
+  "https://prowiki.info/index.php?title=List_of_Pok%C3%A9mon_by_Rarity_Tier"
+) %>% 
+  html_table(fill = TRUE)
 
-# isolate the tables for eacg gen I pokemon's rarity
+# isolate the tables for each gen I pokemon's rarity
 # isolate lookup table for text versions of rarity value
 
-pokemon_tiers <- pro_tables[[3]] %>% clean_names()
-rarity_tiers <- pro_tables[[1]] %>% rename(tier = Tier, rarity_text = name)
+rarity <- rare_tables[[3]] %>% clean_names()
+rarity_lookup <- rare_tables[[1]] %>% rename(tier = Tier, rarity_text = Name)
 
 
-# Join rarity -------------------------------------------------------------
+# Get game exclusives -----------------------------------------------------
+
+
+exclusives <- data_frame(
+  name = c(
+    # red exclusive
+    "Ekans", "Arbok", "Oddish", "Gloom", "Vileplume", "Mankey", "Primeape",
+    "Growlithe", "Arcanine", "Scyther", "Electabuzz",
+    # blue exclusive
+    "Sandshrew", "Sandslash", "Bellsprout", "Weepinbell", "Victreebel",
+    "Meowth", "Persian", "Vulpix", "Ninetales", "Pinsir", "Magma"
+  ),
+  excusive_to = c(rep("red", 11), rep("blue", 11))
+)
+
+
+# Join data ---------------------------------------------------------------
+
 
 # join the rarity tier values to the catch rates data
 # then join the lookup table for tier values to tier text descriptions
 
-pkmn_catch_rarity <- left_join(
+pkmn <- left_join(
   x = catch_rates,
-  y = select(pokemon_tiers, x_no, rarity_tier),
+  y = select(rarity, x_no, rarity_tier),
   by = c("number" = "x_no")
 ) %>% 
   left_join(
-    y = rarity_tiers,
+    y = rarity_lookup,
     by = c("rarity_tier" = "tier")
+  ) %>% 
+  left_join(
+    y = exclusives,
+    by = "name"
   )
